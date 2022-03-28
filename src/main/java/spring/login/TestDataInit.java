34455e1d9c1328bf3ac9131b5205d1622818a077
seongjin_song <seongjin_song@tmax.co.kr>
2022-03-28 20:16:03 +0900

    회원 가입

--- src/main/java/spring/login/TestDataInit.java
+++ src/main/java/spring/login/TestDataInit.java
@@ -4,6 +4,8 @@ import spring.login.domain.item.Item;
 import spring.login.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import spring.login.domain.member.Member;
+import spring.login.domain.member.MemberRepository;
 
 import javax.annotation.PostConstruct;
 
@@ -12,6 +14,7 @@ import javax.annotation.PostConstruct;
 public class TestDataInit {
 
     private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
 
     /**
      * 테스트용 데이터 추가
@@ -20,6 +23,13 @@ public class TestDataInit {
     public void init() {
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 20000, 20));
+
+        Member member = new Member();
+        member.setLoginId("test");
+        member.setPassword("test!");
+        member.setName("테스터");
+
+        memberRepository.save(member);
     }
 
 }
