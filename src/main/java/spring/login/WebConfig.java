ff11c8729ed6519095f41aab5c427a10c3cb2521
seongjin_song <seongjin_song@tmax.co.kr>
2022-03-29 23:25:50 +0900

    서블릿 필터 - 요청 로그

---
+++ src/main/java/spring/login/WebConfig.java
@@ -0,0 +1,22 @@
+package spring.login;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import spring.login.web.filter.LogFilter;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public FilterRegistrationBean logFilter(){
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+}
