4078d671283686e81fe846a0b604e12638b50b70
seongjin_song <seongjin_song@tmax.co.kr>
2022-03-30 00:18:52 +0900

    스프링 인터셉터 - 요청 로그

--- src/main/java/spring/login/WebConfig.java
+++ src/main/java/spring/login/WebConfig.java
@@ -4,14 +4,26 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import spring.login.web.filter.LogFilter;
 import spring.login.web.filter.LoginCheckFilter;
+import spring.login.web.interceptor.LogInterceptor;
 
 import javax.servlet.Filter;
 
 @Configuration
-public class WebConfig {
-    @Bean
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
+    }
+
+//    @Bean
     public FilterRegistrationBean logFilter(){
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
